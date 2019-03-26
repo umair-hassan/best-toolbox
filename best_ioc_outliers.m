30812de11a0cd0e3572ab9328327096946f2f8d1
Umair Hassan <umair.hassan@drz-mainz.de>
2019-03-26 23:02:13 +0100

    Create best_ioc_outliers.m

---
+++ best_ioc_outliers.m
@@ -0,0 +1,25 @@
+function obj=best_ioc_outliers(obj)
+            % function best_ioc_outliers removes the outliers in the MEPs collected using
+            % Levenberg-Marquardt (LM) algorithm and iterative reweighted least squares method
+            
+            %% Data management
+            data=[obj.SI,obj.MEP];
+            data_sort=sortrows(data);
+            s1=data_sort(:,1); m1=data_sort(:,2);
+            
+            
+            %% Outliers detection
+            outliers = isoutlier(m1,'movmean',15); %TODO Make 15 relative to # of trials
+            index_outliers=find(outliers==1);
+            
+            
+            %% Outliers removal from data
+            m1(index_outliers)=[];
+            s1(index_outliers)=[];
+            
+            obj.SI_clean=s1;
+            obj.MEP_clean=m1;
+            
+            
+            
+        end
