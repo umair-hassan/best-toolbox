77e5c6f96991ae8c59b217c2d105dd984a63159c
umair_hassan <umair.hassan@drz-mainz.de>
2019-11-30 19:57:26 +0100

    Create best_main.m

---
+++ best_sub_directory/best_main.m
@@ -0,0 +1,42 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% BEST Main class
+% This class defines a main object for entire toolbox functions
+%
+% by Ing. Umair Hassan (umair.hassan@drz-mainz.de)
+% last edited 2019/02/15 by UH
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+classdef best_main < handle
+    
+    properties
+        
+        curve;      % fitted curve (x,y) points goes here
+        fitresult;  % results of fitted equation parameters goes here
+        gof;        % results of fit goodnes rsquare, rmse etc goes here
+        SI;         % stimulation intensities object values
+        MEP;        % mep's object values
+        SEM;        % standard error of mean (sem) object values
+        ip_x;       % Inflection Point
+        pt_x;       % Plateau
+        th;         % Threshold
+        MEP_clean;  % Outliers removed raw MEP values
+        SI_clean;   % Corrosponding outliers removed raw SI values for MEPs
+        MEP_Descriptives;
+        trial;
+        p;
+        
+    end
+    
+    methods
+        
+        function obj= best_main ()
+            
+            
+        end
+        
+    end
+end
+        
