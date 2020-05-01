b8d74f998bc358569900d341b0d8a10e6d95d349
Umair Hassan <umair.hassan@drz-mainz.de>
2020-05-02 01:52:07 +0200

    feature addition: max likelihood estimation added in psychmth

--- best_toolbox.m
+++ best_toolbox.m
@@ -1011,6 +1011,7 @@ classdef best_toolbox < handle
                             obj.inputs.measure_str='Psychometric Threshold Hunting Protocol';
                             obj.inputs.stop_event=0;
                             obj.inputs.mt_starting_stim_inten=obj.inputs.condsAll.cond1.st1.si_pckt{1,1};
+                            obj.inputs.Handles.ThresholdData=struct;
                             %% Creating Column Labels
                             obj.inputs.colLabel.inputDevices=1;
                             obj.inputs.colLabel.outputDevices=2;
@@ -1135,6 +1136,7 @@ classdef best_toolbox < handle
                             obj.inputs.measure_str='Psychometric Threshold Hunting Protocol';
                             obj.inputs.stop_event=0;
                             obj.inputs.mt_starting_stim_inten=obj.inputs.condsAll.cond1.st1.si_pckt{1,1};
+                            obj.inputs.Handles.ThresholdData=struct;
                             %% Creating Column Labels
                             obj.inputs.colLabel.inputDevices=1;
                             obj.inputs.colLabel.outputDevices=2;
@@ -2336,7 +2338,12 @@ classdef best_toolbox < handle
                     case 'TriggerLockedEEG'
                         obj.PlotTriggerLockedEEG;
                     case 'Psychometric Threshold Trace'
-                        obj.psych_threshold;
+                        switch obj.inputs.ThresholdMethod
+                            case 1
+                                obj.psych_threshold;
+                            case 2
+                                obj.psych_threshold_MLE;
+                        end
                         obj.psych_threshold_trace_plot;
                     case 'StatusTable'
                         obj.StatusTable;
@@ -3206,7 +3213,7 @@ classdef best_toolbox < handle
             %             if first trial, read from starting intensity
             %                 otherwise read from the evokness and write to the
             %                 intensity function
-            AllConditionsFirstTrial=1:obj.inputs.totalConds
+            AllConditionsFirstTrial=1:obj.inputs.totalConds;
             mrk=['mrk' num2str(obj.inputs.trialMat{obj.inputs.trial,obj.inputs.colLabel.marker})];
             if((obj.inputs.trial<=max(AllConditionsFirstTrial)))
                 experimental_condition = [];
@@ -3456,6 +3463,112 @@ classdef best_toolbox < handle
                     TrialToUpdate=TrialsNoForThisMarker(TrialToUpdate(1));
                     obj.inputs.trialMat{TrialToUpdate,obj.inputs.colLabel.si}{1,1}{1,1}=obj.tc.(mrk).stimvalue(StimDevice);
                 end            
+        end
+        function psych_threshold_MLE(obj)
+            mrk=['mrk' num2str(obj.inputs.trialMat{obj.inputs.trial,obj.inputs.colLabel.marker})];
+            
+            if ~isfield(obj.inputs.Handles.ThresholdData,mrk)
+                [~,obj.inputs.Handles.ThresholdData.(mrk).L]=MT_initialize_likelihood_psychmth(obj.inputs.trialMat{obj.inputs.trial,obj.inputs.colLabel.si}{1,1}{1,1});
+            end
+            MEPP2PAmpNonZeroIndex=find(obj.inputs.rawData.(obj.inputs.trialMat{obj.inputs.trial,obj.inputs.colLabel.chLab}{1,obj.inputs.chLab_idx}).data,1,'last');
+            MEPP2PAmp=obj.inputs.rawData.(obj.inputs.trialMat{obj.inputs.trial,obj.inputs.colLabel.chLab}{1,obj.inputs.chLab_idx}).data(MEPP2PAmpNonZeroIndex);
+            [nextInt,obj.inputs.Handles.ThresholdData.(mrk).L]=MT_update_likelihood_psychmth(MEPP2PAmp,obj.inputs.Handles.ThresholdData.(mrk).L);
+            ConditionMarker=obj.inputs.trialMat{obj.inputs.trial,obj.inputs.colLabel.marker};
+            TrialsNoForThisMarker=find(vertcat(obj.inputs.trialMat{1:end,obj.inputs.colLabel.marker})==ConditionMarker);
+            TrialToUpdate=find(TrialsNoForThisMarker>obj.inputs.trial);
+            if ~isempty(TrialToUpdate)
+                TrialToUpdate=TrialsNoForThisMarker(TrialToUpdate(1));
+                obj.inputs.trialMat{TrialToUpdate,obj.inputs.colLabel.si}{1,1}{1,1}=nextInt;
+            end
+            function [nextInt, L] = MT_initialize_likelihood_psychmth(startInt)
+                
+                % Custom cdf where 'm' is mean and '0.07*m' is predifined variance
+                cdfFormula = @(m) normcdf(0:0.1:10,m,0.07*m);
+                
+                % this is the cdf I am trying to emulate
+                realCdf = zeros(2,101);
+                spot = 1;
+                for i = 0:0.1:10
+                    realCdf(1,spot) = i;
+                    spot = spot + 1;
+                end
+                if startInt<1
+                    startInt=1;
+                end
+                central=startInt;
+                first=(central*10+1)-10;
+                last=(central*10+1)+10;
+                
+                realCdf(2,:) = normcdf(0:0.1:10,central,0.07*central);
+                
+                %% Log likelihood function
+                L = zeros(2,101);
+                spot = 1;
+                for i = 0:0.1:10
+                    L(1,spot) = i;
+                    spot = spot + 1;
+                end
+                %% Start with hit at 100% intensity and miss at 0% intensity
+                spot = 1;
+                for i = 0:0.1:10 % go through all possible intensities
+                    thisCdf = cdfFormula(i);
+                    % calculate log likelihood function
+                    L(2,spot) = log(thisCdf(last)) + log(1-thisCdf(first));
+                    spot = spot + 1;
+                end
+                
+                %%
+                
+                %find max values, returns intensity (no indice problem)
+                maxValues = L(1,find(L(2,:) == max(L(2,:))));
+                
+                % Middle Value from maxValues
+                nextInt = round((min(maxValues) + max(maxValues))/2,1);
+                % nextInt=sprintf('%.1f', nextInt)
+                
+                
+            end
+            function [nextInt, L] = MT_update_likelihood_psychmth(MEP, L)
+                
+                % Custom cdf where 'm' is mean and '0.07*m' is predifined variance
+                cdfFormula = @(m) normcdf(0:0.1:10,m,0.07*m);
+                
+                
+                if MEP ==1
+                    disp('Hit')
+                    evokedMEP = 1;
+                else
+                    disp('Miss')
+                    evokedMEP = 0;
+                    
+                end
+                
+                %find max values
+                maxValues = L(1,find(L(2,:) == max(L(2,:))));
+                % Middle Value from maxValues
+                nextInt = round((min(maxValues) + max(maxValues))/2,1);
+
+                
+                % calculate updated log likelihood function
+                spot = 1;
+                for i = 0:0.1:10 % go through all possible intensities
+                    thisCdf = cdfFormula(i);
+                    central=nextInt*10+1;
+                    
+                    if evokedMEP == 1 % hit!
+                        L(2,spot) = L(2,spot) + 1*log(thisCdf(central));
+                    elseif evokedMEP == 0 % miss!
+                        L(2,spot) = L(2,spot) + 1*log(1-thisCdf(central));
+                    end
+                    spot = spot + 1;
+                end
+                %find max values
+                maxValues = L(1,find(L(2,:) == max(L(2,:))));
+                % Middle Value from maxValues
+                nextInt = round((min(maxValues) + max(maxValues))/2,1);
+                %nextInt = maxValues(round(length(maxValues)/2));
+                %display(sprintf('using next intensity: %.2f', nextInt))
+            end
         end
         function mep_threshold_trace_plot(obj)
             ax=['ax' num2str(obj.inputs.trialMat{obj.inputs.trial,obj.inputs.colLabel.axesno}{1,obj.inputs.chLab_idx})];
@@ -3952,109 +4065,7 @@ nextInt = round((min(maxValues) + max(maxValues)) / 2);
 display(sprintf('using next intensity: %.2f', nextInt))
 
         end
-        function [nextInt, L] = MT_initialize_likelihood_psychmth()
-
-% Custom cdf where 'm' is mean and '0.07*m' is predifined variance
-cdfFormula = @(m) normcdf(0:0.1:10,m,0.07*m);
-
-% this is the cdf I am trying to emulate
-realCdf = zeros(2,101);
-spot = 1;
-for i = 0:0.1:10
-    realCdf(1,spot) = i; 
-    spot = spot + 1;
-end
-
-central=3;
-first=(central*10+1)-10;
-last=(central*10+1)+10;
-
-realCdf(2,:) = normcdf(0:0.1:10,central,0.07*central);
-
-%% Log likelihood function
-L = zeros(2,101);
-spot = 1;
-for i = 0:0.1:10
-    L(1,spot) = i;
-    spot = spot + 1;
-end
-%% Start with hit at 100% intensity and miss at 0% intensity
-spot = 1;
-for i = 0:0.1:10 % go through all possible intensities
-    thisCdf = cdfFormula(i);
-    % calculate log likelihood function
-    L(2,spot) = log(thisCdf(last)) + log(1-thisCdf(first));
-    spot = spot + 1;
-end
-
-%%
-
-%find max values, returns intensity (no indice problem)
-maxValues = L(1,find(L(2,:) == max(L(2,:))));
-
-% Middle Value from maxValues
-nextInt = round((min(maxValues) + max(maxValues))/2,1);
-% nextInt=sprintf('%.1f', nextInt)
-
-
-        end
-        function [nextInt, L] = MT_update_likelihood_psychmth(MEP, L, factor)
-
-% Custom cdf where 'm' is mean and '0.07*m' is predifined variance
-cdfFormula = @(m) normcdf(0:0.1:10,m,0.07*m);
-
-
-if MEP > 50
-    disp('Hit')
-    evokedMEP = 1;
-else
-    disp('Miss')
-    evokedMEP = 0;
-
-end
-
-%find max values
-maxValues = L(1,find(L(2,:) == max(L(2,:))));
-% Middle Value from maxValues
-nextInt = round((min(maxValues) + max(maxValues))/2,1);
-if nextInt==0.1
-    nextInt=0.4;
-end
-
-if nextInt==10
-    nextInt=9;
-end
-% nextInt=sprintf('%.1f', maxValues)
-
-%nextInt = maxValues(round(length(maxValues)/2));
-    
-% calculate updated log likelihood function
-spot = 1;
-for i = 0:0.1:10 % go through all possible intensities
-    thisCdf = cdfFormula(i);
-    central=nextInt*10+1;
-
-    if evokedMEP == 1 % hit!
-        L(2,spot) = L(2,spot) + factor*log(thisCdf(central));
-    elseif evokedMEP == 0 % miss!
-        L(2,spot) = L(2,spot) + factor*log(1-thisCdf(central));
-    end
-    spot = spot + 1;
-end
-
-%find max values
-maxValues = L(1,find(L(2,:) == max(L(2,:))));
-maxValues
-% Middle Value from maxValues
-nextInt = round((min(maxValues) + max(maxValues))/2,1);
-% nextInt=sprintf('%.1f', maxValues);
-
-%nextInt = maxValues(round(length(maxValues)/2));
-    
-
-display(sprintf('using next intensity: %.2f', nextInt))
-
-    end
+        
         
         %% Old Scripts
         function best_motorhotspot(obj)
