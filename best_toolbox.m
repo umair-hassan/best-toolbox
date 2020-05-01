fde44c2ddfa28e2c444b95639329ecf6bf77a2bf
Umair Hassan <umair.hassan@drz-mainz.de>
2020-05-01 02:50:54 +0200

    Log Likelihood method ground truth established

--- best_toolbox.m
+++ best_toolbox.m
@@ -3862,6 +3862,200 @@ classdef best_toolbox < handle
             function CloseReqFcn, end
         end
         
+        function [nextInt, L] = MT_initialize_likelihood_mth()
+
+% Custom cdf where 'm' is mean and '0.07*m' is predifined variance
+cdfFormula = @(m) normcdf(0:0.5:100,m,0.07*m);
+
+% this is the cdf I am trying to emulate
+realCdf = zeros(2,201);
+spot = 1;
+for i = 0:0.5:100
+    realCdf(1,spot) = i; 
+    spot = spot + 1;
+end
+
+central=40;
+first=(central*2)-20+1;
+% first=1;
+last=(central*2)+20+1;
+
+realCdf(2,:) = normcdf(0:0.5:100,central,0.07*central);
+
+%% Log likelihood function
+L = zeros(2,201);
+spot = 1;
+for i = 0:0.5:100
+    L(1,spot) = i;
+    spot = spot + 1;
+end
+%% Start with hit at 100% intensity and miss at 0% intensity
+spot = 1;
+for i = 0:0.5:100 % go through all possible intensities
+    thisCdf = cdfFormula(i);
+    % calculate log likelihood function
+    L(2,spot) = log(thisCdf(last)) + log(1-thisCdf(first));
+    spot = spot + 1;
+end
+
+%%
+
+%find max values
+maxValues = L(1,find(L(2,:) == max(L(2,:))));
+% Middle Value from maxValues
+nextInt = round((min(maxValues) + max(maxValues)) / 2);
+%nextInt = maxValues(round(length(maxValues)/2));
+    
+
+        end
+        function [nextInt, L] = MT_update_likelihood_mth(MEP, L, factor)
+
+% Custom cdf where 'm' is mean and '0.07*m' is predifined variance
+m=1
+cdfFormula = @(m) normcdf(0:0.5:100,m,0.07*m);
+
+
+if MEP > 50
+    disp('Hit')
+    evokedMEP = 1;
+else
+    disp('Miss')
+    evokedMEP = 0;
+
+end
+
+%find max values
+maxValues = L(1,find(L(2,:) == max(L(2,:))));
+% Middle Value from maxValues
+nextInt = round((min(maxValues) + max(maxValues)) / 2);
+%nextInt = maxValues(round(length(maxValues)/2));
+    
+% calculate updated log likelihood function
+spot = 1;
+for i = 0:0.5:100 % go through all possible intensities
+    thisCdf = cdfFormula(i);
+    if evokedMEP == 1 % hit!
+        L(2,spot) = L(2,spot) + factor*log(thisCdf(2*nextInt+1));
+    elseif evokedMEP == 0 % miss!
+        L(2,spot) = L(2,spot) + factor*log(1-thisCdf(2*nextInt+1));
+    end
+    spot = spot + 1;
+end
+
+%find max values
+maxValues = L(1,find(L(2,:) == max(L(2,:))));
+% Middle Value from maxValues
+nextInt = round((min(maxValues) + max(maxValues)) / 2);
+%nextInt = maxValues(round(length(maxValues)/2));
+    
+
+display(sprintf('using next intensity: %.2f', nextInt))
+
+        end
+        function [nextInt, L] = MT_initialize_likelihood_psychmth()
+
+% Custom cdf where 'm' is mean and '0.07*m' is predifined variance
+cdfFormula = @(m) normcdf(0:0.1:10,m,0.07*m);
+
+% this is the cdf I am trying to emulate
+realCdf = zeros(2,101);
+spot = 1;
+for i = 0:0.1:10
+    realCdf(1,spot) = i; 
+    spot = spot + 1;
+end
+
+central=3;
+first=(central*10+1)-10;
+last=(central*10+1)+10;
+
+realCdf(2,:) = normcdf(0:0.1:10,central,0.07*central);
+
+%% Log likelihood function
+L = zeros(2,101);
+spot = 1;
+for i = 0:0.1:10
+    L(1,spot) = i;
+    spot = spot + 1;
+end
+%% Start with hit at 100% intensity and miss at 0% intensity
+spot = 1;
+for i = 0:0.1:10 % go through all possible intensities
+    thisCdf = cdfFormula(i);
+    % calculate log likelihood function
+    L(2,spot) = log(thisCdf(last)) + log(1-thisCdf(first));
+    spot = spot + 1;
+end
+
+%%
+
+%find max values, returns intensity (no indice problem)
+maxValues = L(1,find(L(2,:) == max(L(2,:))));
+
+% Middle Value from maxValues
+nextInt = round((min(maxValues) + max(maxValues))/2,1);
+% nextInt=sprintf('%.1f', nextInt)
+
+
+        end
+        function [nextInt, L] = MT_update_likelihood_psychmth(MEP, L, factor)
+
+% Custom cdf where 'm' is mean and '0.07*m' is predifined variance
+cdfFormula = @(m) normcdf(0:0.1:10,m,0.07*m);
+
+
+if MEP > 50
+    disp('Hit')
+    evokedMEP = 1;
+else
+    disp('Miss')
+    evokedMEP = 0;
+
+end
+
+%find max values
+maxValues = L(1,find(L(2,:) == max(L(2,:))));
+% Middle Value from maxValues
+nextInt = round((min(maxValues) + max(maxValues))/2,1);
+if nextInt==0.1
+    nextInt=0.4;
+end
+
+if nextInt==10
+    nextInt=9;
+end
+% nextInt=sprintf('%.1f', maxValues)
+
+%nextInt = maxValues(round(length(maxValues)/2));
+    
+% calculate updated log likelihood function
+spot = 1;
+for i = 0:0.1:10 % go through all possible intensities
+    thisCdf = cdfFormula(i);
+    central=nextInt*10+1;
+
+    if evokedMEP == 1 % hit!
+        L(2,spot) = L(2,spot) + factor*log(thisCdf(central));
+    elseif evokedMEP == 0 % miss!
+        L(2,spot) = L(2,spot) + factor*log(1-thisCdf(central));
+    end
+    spot = spot + 1;
+end
+
+%find max values
+maxValues = L(1,find(L(2,:) == max(L(2,:))));
+maxValues
+% Middle Value from maxValues
+nextInt = round((min(maxValues) + max(maxValues))/2,1);
+% nextInt=sprintf('%.1f', maxValues);
+
+%nextInt = maxValues(round(length(maxValues)/2));
+    
+
+display(sprintf('using next intensity: %.2f', nextInt))
+
+    end
+        
         %% Old Scripts
         function best_motorhotspot(obj)
             obj.info.method=obj.info.method+1;
