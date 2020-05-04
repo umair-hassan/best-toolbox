b151c9b506310444da39372c756ea21341db1dee
Umair Hassan <umair.hassan@drz-mainz.de>
2020-05-04 14:38:02 +0200

    Born: best_fieldtrip

---
+++ best_fieldtrip.m
@@ -0,0 +1,31 @@
+classdef best_fieldtrip <handle    
+    properties
+        best_toolbox
+        fieldtrip
+    end
+    
+    methods
+        function obj = best_fieldtrip(best_toolbox)
+            obj.best_toolbox=best_toolbox;
+        end
+        
+        function setManualAmplitude(obj,Intensity,Stimulator)
+            String=['Set the Intensity of stimulator ''' Stimulator ''' to ''' num2str(Intensity) ''' mA, then click "Done" button.'];
+            f=figure('Name','Digitimer | BEST Toolbox','numbertitle', 'off','ToolBar', 'none','MenuBar', 'none','WindowStyle', 'modal','Units', 'normal', 'Position', [0.5 0.5 .35 .05],'Resize','off','CloseRequestFcn',@(~,~)CloseReqFcn);
+            uicontrol( 'Style','text','Parent', f,'String',String,'FontSize',12,'HorizontalAlignment','center','Units','normalized','Position',[0.05 0.5 1 0.4]);
+            uicontrol( 'Style','pushbutton','Parent', f,'String','Done','FontSize',11,'HorizontalAlignment','center','Units','normalized','Position',[0.4 0.05 0.2 0.4],'Callback',@(~,~)Done);
+            waitfor(f)
+            function Done
+                disp clicked!!
+                delete(f)
+            end
+            function CloseReqFcn
+            end
+        end
+        
+        function setAutomaticAmplitude(obj,Intensity)
+        end
+        
+    end
+end
+
