2a53b8bd3e1fda38b6e8593cf9acbd420319ccdc
Umair Hassan <umair.hassan@drz-mainz.de>
2020-06-01 23:06:01 +0200

    Tidyup

--- BEST.m
+++ BEST.m
@@ -4,13 +4,13 @@ function BEST
 PreviousSession=findobj('Tag','BESTToolboxApplication');
 if ~isempty(PreviousSession)
     answer = questdlg('A new BEST Toolbox Application session was just called, do you want to proceed to a new session by closing current one? Note that this session will be saved and cleared if so.','BEST Toolbox','No','Yes, Open New','No');
-                switch answer
-                    case 'Yes, Open New'
-                        evalin( 'base', 'best_toolbox=best_application;' )
-                    case 'No'
-                        return
-                end
+    switch answer
+        case 'Yes, Open New'
+            evalin( 'base', 'best_toolbox=best_application;' )
+        case 'No'
+            return
+    end
 else
-%% Creating Application
-evalin( 'base', 'best_toolbox=best_application;' )
+    %% Creating Application
+    evalin( 'base', 'best_toolbox=best_application;' )
 end
